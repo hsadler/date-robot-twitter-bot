95cb6876d49dc05ec07f0f73b17338ba3bded0fd
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-04 13:10:05 -0800

    build simple bot

--- bot.py
+++ bot.py
@@ -4,8 +4,11 @@
 import config
 
 import tweepy
-import time
-import sys
+import random
+from datetime import datetime
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
@@ -13,12 +16,63 @@ auth.set_access_token(config.access_key, config.access_secret)
 api = tweepy.API(auth)
 
 
-argfile = str(sys.argv[1])
+month_map = {
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+}
 
 
-with open(argfile,'r') as filename:
-    f = filename.readlines()
+messages = [
+    'Happy day!',
+    'Have a nice day!',
+    'How\'s your day going?',
+    'One of the best days of the year!',
+    'Remember to smile today!',
+    'Today is yesterday\'s tomorrow!',
+    'Today will be tomorrow\'s yesterday!',
+    'Another day!',
+    'Today is great!',
+    'What\'s the news today?',
+    'The week\'s going by so fast! Enjoy today!',
+    'Enjoy your time on this earth. Especially today!'
+]
 
-for line in f:
-    api.update_status(line)
-    time.sleep(10)
+
+# get current date string
+datetime_now = datetime.now()
+
+month_int = datetime_now.strftime('%m')
+month_str = month_map[str(int(month_int))]
+
+day_int = datetime_now.strftime('%d')
+day_str = str(int(day_int))
+
+
+# construct wiki url
+wiki_link = 'https://en.wikipedia.org/wiki/' + month_str + '_' + day_str
+
+
+# commit tweet
+tweet_components = [
+    random.choice(messages),
+    month_str,
+    day_str + ':',
+    wiki_link
+]
+
+api.update_status(' '.join(tweet_components))
+
+
+
+
+
